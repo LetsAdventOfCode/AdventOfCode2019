85bfceaf2bede05951fbcbd93a6c0611f3f33d30
Karl Niklasson <kalle@aotp.se>
2019-12-08 03:08:30 +0100

    tried to make day4 somewhat more efficient by making large skips when the loop hits a combination of trailing 9s, on my input i skips 469257 rounds in the loop

--- Kalle/day4/04.js
+++ Kalle/day4/04.js
@@ -1,42 +1,44 @@
 function solve() {
-    let passwordLimits = document.getElementById("input").value.split("-").map(num => parseInt(num));
-    
+    let passwordLimits = document.getElementById("input").value.split("-");
+    passwordLimits[0] = makeValid(passwordLimits[0]);
 	let dumbPasswords = [];
 	let dumberPasswords = [];
-	bruteforce(passwordLimits, dumbPasswords, dumberPasswords);
+	bruteforce(passwordLimits.map(num => parseInt(num)), dumbPasswords, dumberPasswords);
 	
     document.getElementById("solution").innerHTML = dumbPasswords.length;
 	document.getElementById("solution2").innerHTML = dumberPasswords.length;
 }
 
 function bruteforce(passwordLimits, dumbPasswords, dumberPasswords){
-	for(let i = passwordLimits[0]; i < passwordLimits[1]; i++) {
+	let countChecks = 0;
+	for(let i = passwordLimits[0]; i <= passwordLimits[1]; i++) {
+		countChecks++;
 		let password = i + '';
-		let dupes = {};
-		if(isPasswordValid(password, dupes) && areYouSure(password)){
+		let validityMatching = password.match(/([0-9])\1+/g);
+		if(validityMatching){
 			dumbPasswords.push(i);
 			
-			if(areYouReallyThough(password, dupes.dupes) ){
+			if(validityMatching.some(m => m.length === 2)){
 				dumberPasswords.push(i);
 			}
 		}
+		
+		i += skip(password);
 	}
+	console.log('Passwords to check ' + (passwordLimits[1] - passwordLimits[0]))
+	console.log('Actual number of password checked ' + countChecks)
 }
 
-function isPasswordValid(password, dupes) {
-	let regex = /(.{1}){1}\1/g;
-	dupes.dupes = password.match(regex);
-	return dupes.dupes;
+makeValid = (password) => {
+	let newPassword = [];
+	newPassword[0] = password[0];
+	for (let i = 1; i < password.length; i++){
+		newPassword[i] = password[i] > newPassword[i - 1] ? password[i] : newPassword[i - 1];
+	}
+	return newPassword.join('');
 }
 
-function areYouReallyThough(password, dupes){
-	let tripletsRegexp = /([0-9])\1\1/g;
-	let triplets = password.match(tripletsRegexp);
-	return dupes && dupes.some(dupe => !triplets || triplets.every(triple => dupe[1] != triple[1]))
-}
-
-function areYouSure(password){
-	for(let i = 0; i < password.length - 1; i++)
-		if(password[i] > password[i+1]) return false;
-	return true;
+skip = (password) => {
+	let match = password.match(/9*$/)[0];
+	return match.length > 0 ? (match.replace(new RegExp('9', 'g'), '1')) * (password[password.length - 1 - match.length] * 1 + 1) : 0;
 }
