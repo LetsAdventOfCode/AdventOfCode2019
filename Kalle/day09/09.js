8515d198ba9e5318158c1ca9c3fb51775b0de2d7
Karl Niklasson <kalle@aotp.se>
2019-12-13 01:08:26 +0100

    Managed to lock onto the distress signal from Ceres, the BOOST program is now running

--- Kalle/day09/09.js
+++ Kalle/day09/09.js
@@ -1,40 +1,72 @@
 function solve() {
-    let width = document.getElementById("width").value;
-    let height = document.getElementById("height").value;
-    let input = document.getElementById("input").value;
-    var ctx = document.getElementById('canvas').getContext('2d');
-    let layers = [];
-    let layerSize = width * height;
+    let code = document.getElementById("instructions").value.split(",").map(num => parseInt(num));
+    let input = parseInt(document.getElementById("input").value);
+    let input2 = parseInt(document.getElementById("input2").value);
+    let log = [];
+    let log2 = [];
+    intProgram(code.slice(), input, log);
+    intProgram(code.slice(), input2, log2);
 
-    let lowest = Number.MAX_SAFE_INTEGER;
-    let currentProduct = 0;
+    document.getElementById("solution").innerHTML = log.join();
+    document.getElementById("solution2").innerHTML = log2.join();
+}
 
-    for (let i = 0; i < input.length; i) {
-        let layer = input.slice(i, i + layerSize);
-        let zeroes = layer.match(/[0]/g).length;
-        if (zeroes < lowest) {
-            currentProduct = layer.match(/[1]/g).length * layer.match(/[2]/g).length;
-            lowest = zeroes;
-        }
-        layers.push(layer);
-        i += layerSize;
+var relativeBase = 0;
+
+function intProgram(rows, input, log) {
+    let i = 0;
+    relativeBase = 0;
+    while (i < rows.length) {
+        i = doInstructions(rows, i, input, log);
     }
 
+    return rows;
+}
+
+function doInstructions(code, instructionPointer, input, log) {
+    let operator = code[instructionPointer];
+    let param1 = code[instructionPointer + 1];
+    let param2 = code[instructionPointer + 2];
+    let param3 = code[instructionPointer + 3];
     
-    document.getElementById("solution").innerHTML = currentProduct;
-    drawLayers(layers, ctx, width, height);
-}
-
-function drawLayers(layers, context, width, height) {
-    var cellWidth = canvas.width / width;
-    var cellHeight = canvas.height / height;
-
-    for (var i = 0; i < layers[0].length; i++) {
-        context.fillStyle = cellColor(layers, 0, i);
-        context.fillRect((i % width) * cellWidth, Math.floor(i / width) * cellHeight, cellWidth, cellHeight);
+    let mode = getMode(operator);
+    switch (operator % 100) {
+        case 1:
+            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) + getValue(mode[1], param2, code, relativeBase);
+            return instructionPointer + 4;
+        case 2:
+            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) * getValue(mode[1], param2, code, relativeBase);
+            return instructionPointer + 4;
+        case 3:
+            code[param1 + (mode[2] === '2' ? relativeBase : 0)] = input;
+            return instructionPointer + 2;
+        case 4:
+            log.push(getValue(mode[2], param1, code, relativeBase));
+            return instructionPointer + 2;
+        case 5:
+            return getValue(mode[2], param1, code, relativeBase) !== 0 ? getValue(mode[1], param2, code, relativeBase) : instructionPointer + 3;
+        case 6:
+            return getValue(mode[2], param1, code, relativeBase) === 0 ? getValue(mode[1], param2, code, relativeBase) : instructionPointer + 3;
+        case 7:
+            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) < getValue(mode[1], param2, code, relativeBase) ? 1 : 0;
+            return instructionPointer + 4;
+        case 8:
+            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) === getValue(mode[1], param2, code, relativeBase) ? 1 : 0;
+            return instructionPointer + 4;
+        case 9:
+            relativeBase += getValue(mode[2], param1, code, relativeBase);
+            return instructionPointer + 2;
+        case 99:
+        default:
+            return code.length;
     }
 }
 
-function cellColor(layers, layerIndex, index) {
-    return layers[layerIndex][index] === '2' ? cellColor(layers, layerIndex + 1, index) : layers[layerIndex][index] === '1' ? 'white' : 'black';
+function getValue(mode, param, code, relativeBase) {
+    return mode === '1' ? param : mode === '2' ? (code[param + relativeBase] ? code[param + relativeBase] : 0) : (code[param] ? code[param] : 0);
+}
+
+function getMode(operator) {
+    let mode = (operator / 100).toFixed(0);
+    return mode.length === 1 ? '00' + mode : mode.length === 2 ? '0' + mode : mode;
 }
