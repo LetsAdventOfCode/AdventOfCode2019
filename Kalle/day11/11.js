b3509c4cf526c11ad1677f242dca5e212d51c618
Karl Niklasson <kalle@aotp.se>
2019-12-14 03:42:53 +0100

    removed the local intCodeComputer on the emergency hull painting robot

--- Kalle/day11/11.js
+++ Kalle/day11/11.js
@@ -1,26 +1,27 @@
 function solve() {
     let code = document.getElementById("instructions").value.split(",").map(num => parseInt(num));
     let log = [];
-    intProgram(code.slice(), log);
+    runCode(code.slice(), log);
 }
 
 var relativeBase = 0;
 var input = 0;
 
-function intProgram(rows, log) {
-    let i = 0;    
+function runCode(code, log) {   
     let robot = { x: 0, y: 0, direction: 0 };
     let matrix = createMatrix();
     matrix[robot.y][robot.x] = 1;
-    input = matrix[robot.y][robot.x];
     relativeBase = 0;
     let timeToMove = false;
     let paintCounter = [];
-    while (i < rows.length) {
-        i = doInstructions(rows, i, log);
+    let intCodeComputer = new IntCodeComputer(code);
+    intCodeComputer.input = matrix[robot.y][robot.x];
+
+    while (intCodeComputer.instructionPointer < intCodeComputer.programCode.length) {
+        intCodeComputer.instructionPointer = intCodeComputer.executeInstruction(log);
         if (log.length === 1) {
             if (timeToMove) {
-                input = turnAndMove(matrix, robot, log.shift());
+                intCodeComputer.input = turnAndMove(matrix, robot, log.shift());
                 timeToMove = false;
             }
             else {
@@ -83,51 +84,3 @@ createMatrix = (matrixSize) => {
     }
     return matrix;
 };
-
-function doInstructions(code, instructionPointer, log) {
-    let operator = code[instructionPointer];
-    let param1 = code[instructionPointer + 1];
-    let param2 = code[instructionPointer + 2];
-    let param3 = code[instructionPointer + 3];
-
-    let mode = getMode(operator);
-    switch (operator % 100) {
-        case 1:
-            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) + getValue(mode[1], param2, code, relativeBase);
-            return instructionPointer + 4;
-        case 2:
-            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) * getValue(mode[1], param2, code, relativeBase);
-            return instructionPointer + 4;
-        case 3:
-            code[param1 + (mode[2] === '2' ? relativeBase : 0)] = input;
-            return instructionPointer + 2;
-        case 4:
-            log.push(getValue(mode[2], param1, code, relativeBase));
-            return instructionPointer + 2;
-        case 5:
-            return getValue(mode[2], param1, code, relativeBase) !== 0 ? getValue(mode[1], param2, code, relativeBase) : instructionPointer + 3;
-        case 6:
-            return getValue(mode[2], param1, code, relativeBase) === 0 ? getValue(mode[1], param2, code, relativeBase) : instructionPointer + 3;
-        case 7:
-            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) < getValue(mode[1], param2, code, relativeBase) ? 1 : 0;
-            return instructionPointer + 4;
-        case 8:
-            code[param3 + (mode[0] === '2' ? relativeBase : 0)] = getValue(mode[2], param1, code, relativeBase) === getValue(mode[1], param2, code, relativeBase) ? 1 : 0;
-            return instructionPointer + 4;
-        case 9:
-            relativeBase += getValue(mode[2], param1, code, relativeBase);
-            return instructionPointer + 2;
-        case 99:
-        default:
-            return code.length;
-    }
-}
-
-function getValue(mode, param, code, relativeBase) {
-    return mode === '1' ? param : mode === '2' ? (code[param + relativeBase] ? code[param + relativeBase] : 0) : (code[param] ? code[param] : 0);
-}
-
-function getMode(operator) {
-    let mode = (operator / 100).toFixed(0);
-    return mode.length === 1 ? '00' + mode : mode.length === 2 ? '0' + mode : mode;
-}
