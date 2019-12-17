5bbcb4f49715f0ffbdc883ec7d017bb38dfc60b4
Karl Niklasson <kalle@aotp.se>
2019-12-17 22:01:58 +0100

    Finally....

--- Kalle/day14/14.js
+++ Kalle/day14/14.js
@@ -1,70 +1,51 @@
-var longestPath = 0;
-var tierList = [];
 function solve() {
-    tierList = [];
-    longestPath = 0;
-    let cookBook = document.getElementById("instructions").value.split("\n");
-    recipes = parseCookBook(cookBook);
-    countPath(recipes['FUEL']);
-    let waste = {};
-    let requiredOre = { quantity: 0 };
-    simplify(recipes['FUEL'], requiredOre, waste);
-    let copyWaste = copyAndMultiplyWaste(waste, 1);
-    let returnedOre = { quantity: 0 };
-    for (let i = tierList.length - 1; i >= 0; i--) {
-        for (let key in tierList[i]) {
-            tryReturnWaste(key, copyWaste, returnedOre);
-        }
-    }
-
+    recipes = parseReactions(document.getElementById("instructions").value.split("\n"));
+    let requiredOre = oreToCraftProduct(recipes['FUEL'], 1, {});
     let availableOre = parseInt(document.getElementById("availableOre").value);
-    let maxFuels = calcMaxFuels(availableOre, requiredOre.quantity - returnedOre.quantity, copyWaste);
-
-    document.getElementById("solution").innerHTML = requiredOre.quantity - returnedOre.quantity;
+    let maxFuels = calcMaxFuels(availableOre, requiredOre);
+    document.getElementById("solution").innerHTML = requiredOre;
     document.getElementById("solution2").innerHTML = maxFuels;
 }
 
-function countPath(recipe) {
-    if (recipe.ingredients[0].name === 'ORE') {
-        recipe.tier = 0;
-        if (!tierList[0]) {
-            tierList[0] = [];
-        }
-        tierList[0][recipe.name] = true;
-        return 0;
-    }
-
-    let a = recipe.ingredients.map(i => 1 + countPath(recipes[i.name]));
-    let longestChild = Math.max(...a);
-    if (!tierList[longestChild]) {
-        tierList[longestChild] = [];
-    }
-    recipe.tier = longestChild;
-    tierList[longestChild][recipe.name] = true;
-    return longestChild;
-}
-
-function calcMaxFuels(availableOre, recipeCost, waste) {
+function calcMaxFuels(availableOre, recipeCost) {
     let minimumRepeats = Math.floor(availableOre / recipeCost);
-    console.log(minimumRepeats + ' + ');
-    let newWaste = copyAndMultiplyWaste(waste, minimumRepeats);
-    let returnedOre = { quantity: 0 };
-    for (var i = tierList.length - 1; i >= 0; i--) {
-        for (let key in tierList[i]) {
-            tryReturnWaste(key, newWaste, returnedOre);
+    let waste = {};
+    availableOre -= oreToCraftProduct(recipes['FUEL'], minimumRepeats, waste);
+    let oreAvailable = true;
+    while (oreAvailable) {
+        availableOre -= oreToCraftProduct(recipes['FUEL'], 1, waste);
+        if (availableOre > 0) {
+            minimumRepeats++;
+        } else {
+            oreDepleted = false;
         }
     }
-    let leftOvers = availableOre + returnedOre.quantity - recipeCost * minimumRepeats;
-    return minimumRepeats > 0 ? calcMaxFuels(leftOvers, recipeCost, waste) + minimumRepeats : minimumRepeats;
 }
 
-function copyAndMultiplyWaste(waste, multiplier) {
-    let newWaste = [];
-    for (let i in waste) {
-        newWaste[i] = waste[i] * multiplier;
-    }
-    return newWaste;
+function oreToCraftProduct(recipe, quantityNeeded, waste) {
+    let neededOre = 0;
+    let recipeMultiplier = Math.ceil(quantityNeeded / recipe.quantity);
+
+    recipe.ingredients.forEach(ingredient => {
+        let produced = recipeMultiplier * ingredient.quantity;
+        if (ingredient.name === 'ORE') {
+            neededOre += produced;
+        }
+        else {
+            waste[ingredient.name] = waste[ingredient.name] || 0;
+            if (waste[ingredient.name] < produced) {
+                neededOre += oreToCraftProduct(recipes[ingredient.name], produced - waste[ingredient.name], waste);
+            }
+
+            waste[ingredient.name] = waste[ingredient.name] - produced;
+        }
+    });
+
+    waste[recipe.name] = (waste[recipe.name] || 0) + recipeMultiplier * recipe.quantity;
+
+    return neededOre;
 }
+
 
 function simplify(recipe, requiredOre, waste) {
     for (let ingredient of recipe.ingredients) {
@@ -94,16 +75,16 @@ function tryReturnWaste(component, waste, returnedOre) {
     let recipe = recipes[component];
     if (waste[component]) {
         let canBeSimplifiedTimes = Math.floor(waste[component] / recipe.quantity);
-        
+
         if (canBeSimplifiedTimes) {
             let rest = waste[component] % recipe.quantity;
-            console.log('Replacíng ' + canBeSimplifiedTimes * recipe.quantity + ' ' + recipe.name + ' with ' + recipe.ingredients.map(i => (canBeSimplifiedTimes * i.quantity) + ' ' + i.name).join(', ') + ' leaving ' + rest + ' ' + recipe.name);
+            
             if (rest > 0) {
                 waste[component] = rest;
             } else {
                 delete waste[component];
             }
-            
+
             for (let ingredient of recipe.ingredients) {
                 if (ingredient.name === 'ORE') {
                     returnedOre.quantity += canBeSimplifiedTimes * ingredient.quantity;
@@ -119,14 +100,17 @@ function tryReturnWaste(component, waste, returnedOre) {
     }
 }
 
-function parseCookBook(cookBook) {
+function parseReactions(cookBook) {
     let dict = {};
     for (let recipe of cookBook) {
         let splittedRecipe = recipe.split('=>');
         let ingredients = splittedRecipe[0].split(',').map(i => i.trim()).map(v => createComponent(v));
         let chemical = splittedRecipe[1].split(',').map(i => i.trim()).map(v => createComponent(v))[0];
         if (dict[chemical.name]) {
-            dict[chemical.name].ingredients = ingredients.map();
+            chemical.ingredients = {};
+            for (let ingredient of ingredients) {
+                chemical.ingredients = ingredients;
+            }
         }
         else {
             chemical.ingredients = ingredients;
